ff2532573eb21083ed7186c30646c49daeb494d7
Andrea Salguero <50367726+andreaesalguero@users.noreply.github.com>
2021-09-15 21:05:04 -0300

    cucumber changes

---
+++ cypress/integration/qaCucumber.feature
@@ -0,0 +1,10 @@
+Feature: ML api with cucumber
+
+    As a guest
+    I want to verify how much time i have been using ML platform
+
+    Scenario: ML UI with cucumber
+        Given I am a guest
+        When I submit my ML user
+        And I add my country
+        Then I click on submit and verify the response
---
+++ cypress/integration/qaCucumber/qaCucumberPage.js
@@ -0,0 +1,29 @@
+const user='#nickname'
+const verifyUser='#main-btn'
+const result='#result > p:nth-child(1)'
+const logo='#result > p:nth-child(5) > img'
+const homepage='#result > p:nth-child(8) > a > img'
+
+export class QaCucumber{
+
+    static visit() {
+        cy.visit('https://chcibelli.github.io/meli-age/')
+    }
+
+    static enterUSer(){
+        cy.get(user).type('fravega')
+    }
+
+    static addCountry(){
+        cy.get('select').select('MLA').should('have.value', 'MLA')
+    }
+
+    static submitButton(){
+        cy.get(verifyUser).click()
+        cy.get(result).should('contain', 'Te registraste el 31/07/2013')
+        cy.get(logo).should('be.visible')
+        cy.get(homepage).should('be.visible')
+    }
+
+}
+export default QaCucumber
